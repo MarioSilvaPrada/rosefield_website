349a479def0b7c962c5ef2e54aef829ba8e74969
MarioSilvaPrada <mariosilvaprada@gmail.com>
2019-05-29 23:07:48 +0100

    updating animation

--- src/App.js
+++ src/App.js
@@ -6,15 +6,25 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 import Story from "./components/Story";
 
-function App() {
-  return (
-    <div className="App">
-      <Advertisment />
-      <Header />
-      <Main />
-      <Story />
-    </div>
-  );
+import { animateHTML } from "./config/animation";
+
+class App extends React.Component {
+
+  // Call animation function
+  componentDidMount() {
+    animateHTML().init();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Advertisment />
+        <Header />
+        <Main />
+        <Story />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -6,14 +6,75 @@ import hand from "../config/img/hand.png";
 const StyledStory = styled.div`
   position: absolute;
   display: flex;
+  justify-content: space-between;
   background: white;
   width: 100%;
+
+  .story-text {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    margin-left: 15rem;
+    text-align: left;
+  }
+
+  .story-text :nth-child(1) {
+    font-size: 0.8rem;
+  }
+  .story-text-main {
+    font-size: 3rem;
+  }
+  .story-text-description {
+    line-height: 2rem;
+  }
+  .story-text-discover {
+    align-self: flex-start;
+    padding: 0 1.5rem 0.7rem 0;
+    border-bottom: 1px solid black;
+  }
+
+  ${'' /* .hand{
+    width: 800px;
+    height:auto;
+  } */}
+
+  .hidden {
+    opacity: 0;
+  }
+
+  @keyframes fadeInRight {
+    from {
+      opacity: 0;
+      transform: translate3d(100%, 0, 0);
+    }
+
+    to {
+      opacity: 1;
+      transform: translate3d(0, 0, 0);
+    }
+  }
+
+  .fadeInRight {
+    animation: fadeInRight 2s;
+  }
 `;
 
 const Story = () => (
   <StyledStory>
-    <div>Text</div>
-    <img alt="hand" className="hand" src={hand} />
+    <div className="story-text">
+      <div>Our Story</div>
+      <div className="story-text-main">
+        Where function <br />
+        meets fashion
+      </div>
+      <div className="story-text-description">
+        In short, ROSEFIELD watches are a tribute to the great vibes, the <br />
+        energy and the tolerance for free thinking that is typical for both
+        <br /> Amsterdam and New York City.
+      </div>
+      <div className="story-text-discover">Discover our products</div>
+    </div>
+    <img alt="hand" className="hand hidden" src={hand} />
   </StyledStory>
 );
 
